9531b7ac21fd46a4a83ee6185e75c4ac3692f8e8
ksinghcodes <oeksingh@gmail.com>
2021-03-08 18:22:11 -0500

    First set of codes done

---
+++ helloWorld.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 3.7
+
+##### this is a comment 
+"""
+this is multi line string . not comment . 
+does not execute 
+but takes up memory """
+
+#### variable examples. start with lower case
+
+my_str = ' this is a string'
+my_str += ' , with another string added' 
+str_num = '2'
+num = 3
+
+
+
+# this is a print statement for a variable
+print(my_str)
+print(str_num)
+print(num)
+######   this doe not work print(str_num + num )
