367bf77f1fe01f76059f65957e0714d3b0c8f20b
ksinghcodes <oeksingh@gmail.com>
2021-03-10 14:36:19 -0500

    added strings 2

---
+++ strings2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 3.7
+
+
+#len('value') is fucntion for getting length of string
+#len(my_str)
+#len('testing')
+
+#string indexing and slicing
+test_str='testing'
+
+# Indexing
+# test_str[0] - position of 't' returns 't'
+# test_str[-1] == test_str[len(test_str)-1] returns 'g'
+
+# SLicing
+#test_str[FirstLeter : Letter outside last Letter]
+#test_str[0:2] returns 'te' - positions 0 up to not including 2
+#test_str[2:] - 2 to end 
+#test_str[1:6:2] steps by two - 'etn' e_t_n
+#test_str[:6:2] start at begining to 6 step by 2 'tsi'
+#test_str[1::2] 1 to end step 2 'etn'
+#test_str[::-1] start to end going back step 1 - reverses string ' gnitset'
+
