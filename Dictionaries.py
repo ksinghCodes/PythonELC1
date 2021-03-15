a963ec7e92eeb5a239145ae54719d5ff2eebc05f
ksinghcodes <oeksingh@gmail.com>
2021-03-15 12:27:33 -0400

    more notes

--- Dictionaries.py
+++ Dictionaries.py
@@ -18,6 +18,21 @@ ages = {'Kevin': 59, 'alex': 29, 'bob': 40}
 # Can still use IN/ not in operations
 # alex in ages >>> true
 
-#initialzing
+#initialzing more officially 
 weights = dict(kevin=160, bob=240, kayla=135)
 colors = dict([('kevin', 'blue'), ('bob', 'green'), ('kayla', 'red')])
+
+
+
+# methods 
+#.keys() returns list of all keys
+# .values() returns list of all values 
+ages.keys() #>>> dict_keys(['kevin','bob','kayla'])
+list(ages.keys()) #>>> cast to make clearly list ['kevin' ..e.tc ]
+ages.values()
+list(ages.Values())
+
+# returns items as list of tuples
+ages.items()
+lsit(ages.items())
+
